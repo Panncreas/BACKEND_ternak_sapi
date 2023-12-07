de0a7175e68d8fb313cade4b8ea80a3c4da4d540
senja <aswinaditya007@gmail.com>
2023-12-07 22:54:40 +0700

    auto create file uploads
    auto membuat file uploads in other your db place , so  no worry be happy

--- src/main/java/com/ternak/sapi/service/AuthService.java
+++ src/main/java/com/ternak/sapi/service/AuthService.java
@@ -8,6 +8,7 @@ import com.ternak.sapi.payload.SignUpRequest;
 import com.ternak.sapi.repository.RoleRepository;
 import com.ternak.sapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -15,39 +16,60 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.UUID;
+
 @Service
 public class AuthService {
-    @Autowired
-    UserRepository userRepository;
+
+    @Value("${file.upload-dir}")
+    private String uploadDir;
 
     @Autowired
-    RoleRepository roleRepository;
+    private UserRepository userRepository;
 
     @Autowired
-    PasswordEncoder passwordEncoder;
+    private RoleRepository roleRepository;
 
-    private final String FOLDER_PATH="E:/Myfile/Magang Maching Fund/img"; 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public AuthService(@Value("${file.upload-dir}") String uploadDir) {
+        this.uploadDir = uploadDir;
+        init();
+    }
+
+    public void init() {
+    try {
+        Path path = Paths.get(System.getProperty("user.dir"), "uploads");
+        Files.createDirectories(path);
+    } catch (IOException e) {
+        throw new RuntimeException("Failed to create upload directory", e);
+    }
+}
+
 
     public User signUp(SignUpRequest signUpRequest, MultipartFile file, String selectedRole) throws IOException {
-    String originalFilename = file.getOriginalFilename();
-    String randomName = UUID.randomUUID().toString() + "." + StringUtils.getFilenameExtension(originalFilename);
-    String filePath = FOLDER_PATH + randomName;
-    File destFile = new File(FOLDER_PATH, randomName);
-    
-    // Check if the selected role is valid
-    Role userRole = roleRepository.findByName(RoleName.valueOf(selectedRole))
-            .orElseThrow(() -> new AppException("Invalid User Role."));
+        String originalFilename = file.getOriginalFilename();
+        String randomName = UUID.randomUUID().toString() + "." + StringUtils.getFilenameExtension(originalFilename);
+        String filePath = uploadDir + File.separator + randomName;
+        File destFile = new File(uploadDir, randomName);
 
-    User user = new User(signUpRequest.getName(), signUpRequest.getUsername(),
-            signUpRequest.getEmail(), signUpRequest.getPassword(), filePath);
-    file.transferTo(destFile);
-    user.setPassword(passwordEncoder.encode(user.getPassword()));
-    
-    // Set the user's roles
-    user.setRoles(Collections.singleton(userRole));
+        // Check if the selected role is valid
+        Role userRole = roleRepository.findByName(RoleName.valueOf(selectedRole))
+                .orElseThrow(() -> new AppException("Invalid User Role."));
 
-    return userRepository.save(user);
-}
+        User user = new User(signUpRequest.getName(), signUpRequest.getUsername(),
+                signUpRequest.getEmail(), signUpRequest.getPassword(), filePath);
+        file.transferTo(destFile);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        // Set the user's roles
+        user.setRoles(Collections.singleton(userRole));
+
+        return userRepository.save(user);
+    }
 }
